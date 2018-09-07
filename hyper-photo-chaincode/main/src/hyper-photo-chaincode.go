b11b334c0d89047f00ccb6223e5ceccab9e71c9d
clod16 <c.barberini94@gmail.com>
2018-09-07 14:34:21 +0200

    chaincode upgrade

--- hyper-photo-chaincode/main/src/hyper-photo-chaincode.go
+++ hyper-photo-chaincode/main/src/hyper-photo-chaincode.go
@@ -11,3 +11,49 @@ import(
 
 var logger = shim.NewLogger("HyperPhoto-log")
 
+
+
+func (t *HyperPhoto) Init(stub shim.ChaincodeStubinterface) pb.Response {
+	logger.Info("Initializing Hyper Photo")
+	logger.SetLevel(shim.LogDebug)
+	_, args := stub.GetFunctionAndParameters()
+	//var err error
+
+	// Upgrade Mode 1: leave ledger state as it was
+	if len(args) == 0 {
+		//logger.Info("Args correctly!!!")
+		return shim.Success(nil)
+	}
+
+	return shim.Success(nil)
+}
+
+
+func (t *HyperPhoto) Invoke(stub shim.ChaincodeStubinterface) pb.Response {
+	var err error
+
+
+	function, args := stub.GetFunctionAndParameters()
+	if function == "initPhoto"
+	else if function == "putPhoto" {
+		return t.putPhoto(stub, args)
+	} else if function == "getPhoto" {
+		return t.getPhoto(stub, args)
+	}
+	return shim.Error("Invalid invoke function name")
+}
+
+func (t *HyperPhoto) putPhoto(stub shim.ChaincodeStubinterface) pb.Response{
+	logger.Debug("      putPhoto      ")
+
+	var photoKEY string
+
+
+
+
+
+}
+
+
+
+
--- hyper-photo-chaincode/main/src/photo.go
+++ hyper-photo-chaincode/main/src/photo.go
@@ -2,17 +2,20 @@ package main
 
 
 type Description struct{
-
+	ISO          string `json:"iso"`
+	Exposition   string `json:"exposition"`
+	ShutterSpeed string `json:"shutterSpeed"`
+	Baffle       string `json:"Baffle"`
 	
 }
 
 type Photo struct{
-	Id string
-	Author string
-	Date string
-	Format string
-	
-	Description
+	ID          string `json:"id"`
+	Author      string `json:"author"`
+	Date        string `json:"date"`
+	Format      string `json:"format"`
+	Status      string `json:"status"`
+	Description `json:"description"`
 	
 
 }
